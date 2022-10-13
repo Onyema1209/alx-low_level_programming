0835250cc0f02341fbbdd5b4e7aefd076a0b8b6a
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-13 09:26:13 -0700

    5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) /*just to print with printf*/
+/**
+ *main- print text
+ *Return: return 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
