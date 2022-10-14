e5b4d54bce757a4019d431f1e1a7ef20ff19a3c1
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-14 12:43:34 -0700

    4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- prints the alphabet in lowercase, followed by a new line
+ *Print all the letters except q and e
+ *Return: return (0) if successful
+ */
+int main(void)
+{
+		char c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+
+}
