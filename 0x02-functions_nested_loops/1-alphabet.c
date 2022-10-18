6f636916aa7dba3bea11aa5f6140dd0f94887f32
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-18 14:29:55 -0700

    complited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet- prints the alphabet, in lowercase,
+ *followed by a new line
+ *Return: success
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
