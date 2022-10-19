12812c93f7faa3d4c76e224d24dcad95c9ad6d72
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-18 18:16:03 -0700

    yes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add- adds two integers and returns the result.
+ *@n1: first number test
+ *@n2: second number tet
+ *Return: sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
