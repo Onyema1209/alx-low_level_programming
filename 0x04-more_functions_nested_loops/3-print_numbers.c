9c701f6d5dd251a013d407598f0a338f601cdb8a
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-20 17:56:47 -0700

    lol

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * print_numbers - print from 0 to 9
+ * Description: This function prints digits
+ * from 0-9 but from ASCII charcater 48-57
  * Return: void (nothing)
  */
 void print_numbers(void)
@@ -10,5 +12,5 @@ void print_numbers(void)
 
 	for (num = 48; num <= 57; num++)
 		_putchar(num);
-		_putchar('\n');
+	_putchar('\n');
 }
