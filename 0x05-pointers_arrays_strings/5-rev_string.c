97f53e6810a001a6257f34bda0e4d053410f806e
Onyema1209 <onyemaemmanuel63@gmail.com>
2022-10-25 08:14:32 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: input string
+ * Return: void (nothing)
+ */
+void rev_string(char *s)
+{
+	int left, i, temp;
+
+	left = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > left)
+	{
+		temp = s[i];
+		s[i--] = s[left];
+		s[left++] = temp;
+	}
+}
